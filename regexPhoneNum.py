ae2f71ecfd9983d2493b49d513a70fdaab9f534a
Savan Bhanderi <bsavan414@gmail.com>
2020-03-14 22:07:38 -0400

    Regex findall method

--- regexPhoneNum.py
+++ regexPhoneNum.py
@@ -3,5 +3,5 @@ import re
 message = 'Call me at 578-415-9612 tomorrow, or at 425-874-3678'
 
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phoneNumRegex.search(message)
-print(mo.group())
+mo = phoneNumRegex.findall(message) #This returns a list of all matching items
+print(mo)
